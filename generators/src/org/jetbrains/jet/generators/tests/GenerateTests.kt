48d41043a883b792c6cf25afc57398161730f2b0
Valentin Kipyatkov <valentin@jetbrains.com>
2014-10-24 17:10:11 +0400

    Added test for KT-6132

--- generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
@@ -128,6 +128,7 @@ import org.jetbrains.k2js.test.semantics.AbstractReservedWordTest
 import org.jetbrains.jet.resolve.AbstractReferenceResolveInJavaTest
 import org.jetbrains.k2js.test.semantics.AbstractBridgeTest
 import org.jetbrains.jet.j2k.test.AbstractJavaToKotlinConverterForWebDemoTest
+import org.jetbrains.jet.completion.AbstractMultiFileSmartCompletionTest
 
 fun main(args: Array<String>) {
     System.setProperty("java.awt.headless", "true")
@@ -358,6 +359,13 @@ fun main(args: Array<String>) {
 
         testClass(javaClass<AbstractCodeFragmentCompletionTest>()) {
             model("completion/basic/codeFragments", extension = "kt")
+        }
+
+        testClass(javaClass<AbstractMultiFileJvmBasicCompletionTest>()) {
+            model("completion/basic/multifile", extension = null, recursive = false)
+        }
+        testClass(javaClass<AbstractMultiFileSmartCompletionTest>()) {
+            model("completion/smartMultiFile", extension = null, recursive = false)
         }
 
         testClass(javaClass<AbstractGotoSuperTest>()) {
@@ -602,10 +610,6 @@ fun main(args: Array<String>) {
 
         testClass(javaClass<AbstractStubBuilderTest>()) {
             model("stubs", extension = "kt")
-        }
-
-        testClass(javaClass<AbstractMultiFileJvmBasicCompletionTest>()) {
-            model("completion/basic/multifile", extension = null, recursive = false)
         }
 
         testClass(javaClass<AbstractMultiFileHighlightingTest>()) {
